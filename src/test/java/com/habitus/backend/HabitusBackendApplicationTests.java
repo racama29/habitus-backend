871a6db10bb51666997bd60dad4b15178559e8a7
Rafael.Camacho1 <rafaelcamachomarchena.2324@alumnos.fesac.es>
2025-05-02 12:02:52 +0200

    Initial commit - Spring Boot backend

---
+++ src/test/java/com/habitus/backend/HabitusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.habitus.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HabitusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

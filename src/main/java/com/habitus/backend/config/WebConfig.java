08eaf8f99fe229c75f3259e4e229fe2895ac7c31
Rafael.Camacho1 <rafaelcamachomarchena.2324@alumnos.fesac.es>
2025-05-26 18:33:36 +0200

    link to frontend web

--- src/main/java/com/habitus/backend/config/WebConfig.java
+++ src/main/java/com/habitus/backend/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns("http://localhost:4200")
+                .allowedOrigins("https://habitus-93059.web.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
